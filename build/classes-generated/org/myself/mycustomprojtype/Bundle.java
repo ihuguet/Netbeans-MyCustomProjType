5e6dd850bf1915e439a4ada935f5dcfcbc900e01
Iñigo Huguet <inigohuguet@fanamoel.com>
2017-07-05 10:14:11 +0200

    first commit

---
+++ build/classes-generated/org/myself/mycustomprojtype/Bundle.java
@@ -0,0 +1,13 @@
+package org.myself.mycustomprojtype;
+/** Localizable strings for {@link org.myself.mycustomprojtype}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>My custom project</i>
+     * @see MyCustomProjectTemplateWizardIterator
+     */
+    static String MyCustomProjectTemplate_displayName() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "MyCustomProjectTemplate_displayName");
+    }
+    private void Bundle() {}
+}
